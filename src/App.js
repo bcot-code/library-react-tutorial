3f732beca5ddbc85e1eddaff205198bf26958b28
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-06-03 21:39:54 -0500

    init cart

--- src/App.js
+++ src/App.js
@@ -3,25 +3,52 @@ import Nav from "./components/Nav"
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import BookInfor from "./pages/Bookinfor";
+import Cart from './pages/Cart';
 import Footer from "./components/Footer";
 import { books } from './data';
-import {BrowserRouter as Router, Route, } from 'react-router-dom'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+import { useEffect, useState } from "react";
 
 
 
 function App() {
+  const [cart, setCart] = useState([]);
+  function addToCart(book){
+    setCart([...cart, book])
+  }
+  
+  useEffect(() => {
+    console.log(cart);
+  }, [cart])
+  
   return (
     <Router>
       <div className="App">
         <Nav />
         <Route path ="/" exact component={Home} />
         <Route path ="/books" exact render={() => <Books books={books}/>}/>
-        <Route path ="/books/:id" render={() => <BookInfor books={books}/>}/>
+        <Route path ="/books/:id" render={() => <BookInfor books={books} addToCart={addToCart}/>}/>
+        <Route path ="/cart" render={() => <Cart books={books}/>}/>
         <Footer />
       </div>
     </Router>
     
   );
 }
-
+// const dupItem = cart.find(item => +item.id === +book.id)
+    // if(dupItem){
+    //   setCart(cart.map(item => {
+    //     if(item.id === dupItem.id){
+    //       return{
+    //         ...item,
+    //         quantity: item.quantity + 1,
+    //       }
+    //     } else {
+    //       return item
+    //     }
+    //   }))
+    // } else{
+    //   setCart([...cart, {...book, quantit:1}])
+    // }
+    // this on top david says "this will only confuse the devs? so why make it this way...."
 export default App;
--- src/pages/Bookinfor.jsx
+++ src/pages/Bookinfor.jsx
@@ -5,10 +5,20 @@ import Rating from '../components/ui/rating'
 import Pricing from '../components/ui/pricing'
 import Book from '../components/ui/books'
 
-const Bookinfor = ({books}) => {
+const Bookinfor = ({books, addToCart, cart}) => {
    const {id} = useParams()
-      //in js tries to find the value is a number using (+) 
-      const book = books.find(book => +book.id === +id)
+    //in js tries to find the value is a number using (+) 
+    const book = books.find((book) => +book.id === +id)
+  
+
+    function addBookToCart(book){
+      addToCart(book);
+    }
+
+    function bookExistsOnCart(){
+      return cart.find(book => book.id === +id);
+    }
+
   return (
     <div id='books__body'>
       <main id='books__main'>
@@ -45,9 +55,14 @@ const Bookinfor = ({books}) => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam blanditiis quae rem officia animi laudantium totam omnis quam suscipit. Magnam provident dolorum omnis corporis eveniet molestiae reprehenderit doloremque expedita iure.
                   </p>
                 </div>
-                <button className="btn">
-                  Add to Cart
-                </button>
+                {
+                  bookExistsOnCart() ? (
+                    <Link to={`/cart`} className='book__link'>
+                      <button className='btn'>Checkout</button>
+                    </Link>
+                  ) : (
+                    <button className="btn" onClick={() => addBookToCart(book)}> Add to Cart </button>
+                    )}
               </div>
             </div>
           </div>
@@ -63,8 +78,8 @@ const Bookinfor = ({books}) => {
               {books
                 .filter((book) => book.rating === 5 && +book.id !== +id)
                 .slice(0,4)
-                .map((book) => (<Book book={book} key={book.id} />))
-              }
+                .map((book) => (<Book book={book} key={book.id} />
+                ))}
               </div>
           </div>
         </div>
